89c8ea2c2288d2f18cbf004aacb332677686c1fd
Hsiang307 <hsiang@tibbo.com>
2018-12-27 17:47:45 +0800

    Added humidity sensor innaccuracy warning

--- http-resources/index.js
+++ http-resources/index.js
@@ -2,7 +2,7 @@ class MainMenu extends React.Component {
   constructor() {
     super();
     $.ajaxSetup({ cache: false });
-    this.state = { probetype: null, probename: null, reading: null }
+    this.state = { probetype: null, probename: null, reading: null, tempwarning: null }
     this.updateJSON();
   }
 
@@ -16,8 +16,8 @@ class MainMenu extends React.Component {
       ["Tibbit 35","Barometric Pressure Sensor"]
     ]
     this.interval = setInterval(() => {
-      $.getJSON("json.html?date=" + new Date(), (data) => {
-        this.setState({ probetype: sensorTypes[data.probe][0], probename: sensorTypes[data.probe][1], reading: data.reading })
+      $.getJSON("json.html?date=" + new Date(), (data) => {   
+        this.setState({ probetype: sensorTypes[data.probe][0], probename: sensorTypes[data.probe][1], reading: data.reading, tempwarning: data.warning })
       })
     }, 500);
   }
@@ -28,6 +28,26 @@ class MainMenu extends React.Component {
         <div>
           <br></br><h1>No Probe Detected!</h1> <br></br>
           <h2>Please ensure probe is connected correctly</h2>
+        </div>
+      )
+    }
+    else if (this.state.tempwarning === "0") {
+      return (
+        <div>
+          <br></br><h1>{this.state.probetype}<br></br><br></br>
+          {this.state.probename}<br></br><br></br>
+          <green>{this.state.reading}</green><br></br><br></br></h1>
+          <h2><red>*Humidity sensors are less accurate at temperatures below 5°C</red></h2>
+        </div>
+      )
+    }
+    else if (this.state.tempwarning === "1") {
+      return (
+        <div>
+          <br></br><h1>{this.state.probetype}<br></br><br></br>
+          {this.state.probename}<br></br><br></br>
+          <green>{this.state.reading}</green><br></br><br></br></h1>
+          <h2><red>*Humidity sensors are less accurate at temperatures above 50°C</red></h2>
         </div>
       )
     }
